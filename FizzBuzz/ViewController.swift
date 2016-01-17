c943ed4fed68a34045178a6730fda6eab0b5bce2
Patrick Smith <pjs482@gmail.com>
2016-01-16 22:24:11 -0500

    Initial Commit

---
+++ FizzBuzz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FizzBuzz
+//
+//  Created by Patrick Smith on 1/16/16.
+//  Copyright © 2016 Patrick Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
